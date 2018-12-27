87444f191db36eeb3d00bbcce7cda3eda17736c4
uranus yu <uranusx86@hotmail.com>
2018-12-27 20:57:43 +0800

    numpy control group uses loop unroll method

--- test_performance.py
+++ test_performance.py
@@ -26,6 +26,34 @@ def random_erase_np(img, probability = 0.5, sl = 0.02, sh = 0.4, r1 = 0.3):
 
     return img
 
+def random_erase_np_v2(img, probability = 0.5, sl = 0.02, sh = 0.4, r1 = 0.3):
+    height = img.shape[0]
+    width = img.shape[1]
+    channel = img.shape[2]
+    area = width * height
+
+    erase_area_low_bound = np.round( np.sqrt(sl * area * r1) ).astype(np.int)
+    erase_area_up_bound = np.round( np.sqrt((sh * area) / r1) ).astype(np.int)
+    if erase_area_up_bound < height:
+        h_upper_bound = erase_area_up_bound
+    else:
+        h_upper_bound = height
+    if erase_area_up_bound < width:
+        w_upper_bound = erase_area_up_bound
+    else:
+        w_upper_bound = width
+
+    h = np.random.randint(erase_area_low_bound, h_upper_bound)
+    w = np.random.randint(erase_area_low_bound, w_upper_bound)
+
+    x1 = np.random.randint(0, height+1 - h)
+    y1 = np.random.randint(0, width+1 - w)
+
+    x1 = np.random.randint(0, height - h)
+    y1 = np.random.randint(0, width - w)
+    img[x1:x1+h, y1:y1+w, :] = np.random.randint(0, 255, size=(h, w, channel)).astype(np.uint8)
+    return img
+
 if __name__ == '__main__':
     sess = tf.Session()
     raw_jpeg = tf.read_file("./data/cat.jpg")
@@ -37,7 +65,7 @@ if __name__ == '__main__':
 
     for _ in range(5):
         start = time.time()
-        img1 = tf.py_func(random_erase_np, [image], tf.uint8)
+        img1 = tf.py_func(random_erase_np_v2, [image], tf.uint8)
         imsave("random_erasing_np.jpg", sess.run(img1))
         print("Numpy version: {}".format(time.time()-start))
 
